467ef9b3e80a74151671a7863017bc342690ea10
make-steve <make.alsteve@gmail.com>
2018-05-23 16:26:35 +0800

    update footer template

--- bitrix/templates/webshop/footer.php
+++ bitrix/templates/webshop/footer.php
@@ -102,12 +102,12 @@
     <!-- /.col -->
     <!--Footer end -->
     <!--Core JavaScript file  -->
-    <script src="assets/js/jquery-1.10.2.js"></script>
+    <script src="<?=CUtil::GetAdditionalFileURL(SITE_TEMPLATE_PATH."/assets/js/jquery-1.10.2.js")?>"></script>
     <!--bootstrap JavaScript file  -->
-    <script src="assets/js/bootstrap.js"></script>
+    <script src="<?=CUtil::GetAdditionalFileURL(SITE_TEMPLATE_PATH."/assets/js/bootstrap.js")?>"></script>
     <!--Slider JavaScript file  -->
-    <script src="assets/ItemSlider/js/modernizr.custom.63321.js"></script>
-    <script src="assets/ItemSlider/js/jquery.catslider.js"></script>
+    <script src="<?=CUtil::GetAdditionalFileURL(SITE_TEMPLATE_PATH."/assets/ItemSlider/js/modernizr.custom.63321.js")?>"></script>
+    <script src="<?=CUtil::GetAdditionalFileURL(SITE_TEMPLATE_PATH."/assets/ItemSlider/js/jquery.catslider.js")?>"></script>
     <script>
         $(function () {
 
